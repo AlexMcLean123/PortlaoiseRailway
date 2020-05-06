fc9ee6a897014e235c5c597a1d64ef2a0b68bf03
AlexMcLean123 <alimclean22@gmail.com>
2020-05-06 12:22:00 +0100

    added functionality for my local station

--- trainApplication/src/app/app.component.ts
+++ trainApplication/src/app/app.component.ts
@@ -11,19 +11,55 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 export class AppComponent {
   title = 'trainApplication';
   public xmlItems: any;
-  public xmlItemsReturn: any;
   time = new Date();
   timer;
-  url = "http://api.irishrail.ie/realtime/realtime.asmx/getStationDataByCodeXML_WithNumMins?StationCode=PTLSE&NumMins=90"
+  url = "http://api.irishrail.ie/realtime/realtime.asmx/getStationDataByCodeXML_WithNumMins?StationCode="
+  //these are the stations I actually use so I used these 
+  athyCode = "ATHY"
+  athy = "Athy"
+  portlaoiseCode = "PTLSE"
+  portlaoise = "Portlaoise"
+  carlowCode = "CRLOW"
+  carlow = "Carlow"
+  mins = "&NumMins=90"
+  athyInterval
+  portLoaiseInterval
+  carlowInterval
   constructor(private http: HttpClient) {
 
   }
+  //the request is made automatically every minute, this is used to stop it if the user changes station
+  stopInterval(interval, interval2) {
+    clearInterval(interval);
+    clearInterval(interval2);
+    console.log("intervals Stopped")
+  }
 
 
-  getData() {
-    const data = this.http.get(this.url, { headers: new HttpHeaders({ 'Accept': 'application/xml' }), responseType: 'text' })
+  toggleAthy() {
+    this.stopInterval(this.portLoaiseInterval, this.carlowInterval)
+    this.getData(this.athyCode, this.athy)
+    //makes request every 60 seconds, updates data in html template 
+    this.athyInterval = setInterval(() => { this.getData(this.athyCode, this.athy) }, 60 * 1000);
+  }
+
+  togglePortlaoise() {
+    this.stopInterval(this.athyInterval, this.carlowInterval)
+    this.getData(this.portlaoiseCode, this.portlaoise)
+    this.portLoaiseInterval = setInterval(() => { this.getData(this.portlaoiseCode, this.portlaoise) }, 60 * 1000);
+  }
+
+  toggleCarlow() {
+    this.stopInterval(this.portLoaiseInterval, this.athyInterval)
+    this.getData(this.carlowCode, this.carlow)
+    this.carlowInterval = setInterval(() => { this.getData(this.carlowCode, this.carlow) }, 60 * 1000);
+  }
+
+  getData(stationCode, stationName) {
+    const data = this.http.get(this.url + stationCode + this.mins, { headers: new HttpHeaders({ 'Accept': 'application/xml' }), responseType: 'text' })
+    console.log(this.url + stationCode + this.mins)
     data.subscribe(xmlData => {
-      this.parseXML(xmlData, "Portlaoise")
+      this.parseXML(xmlData, stationName)
         .then((xmlData) => {
           this.xmlItems = xmlData;
         });
@@ -45,6 +81,7 @@ export class AppComponent {
         var obj = result.ArrayOfObjStationData;
         for (k in obj.objStationData) {
           var item = obj.objStationData[k];
+          //filtered to only show trains going from the station, thought it made more sense to do so
           if (item.Destination != station) {
             arr.push({
               dueIn: item.Duein[0],
@@ -63,11 +100,9 @@ export class AppComponent {
   }
 
   ngOnInit() {
-    this.getData()
     this.timer = setInterval(() => {
       this.time = new Date();
     }, 1000)
-    setInterval(() => { this.getData() }, 60 * 1000);
   }
 
   ngOnDestroy() {
